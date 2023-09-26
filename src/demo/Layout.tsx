06fed8a7a618b8de94ee9ed2379e811c61cfcab9
nate-hunter <hunter.j.nate@gmail.com>
2023-09-26 01:06:57 -0400

    created `UserContext` in `Layout`

--- src/demo/Layout.tsx
+++ src/demo/Layout.tsx
@@ -1,11 +1,20 @@
+import { createContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { SupashipUserInfo, useSession } from '../hooks/use-session';
 import { NavBar } from './NavBar';
 
+export const UserContext = createContext<SupashipUserInfo>({
+  session: null,
+  profile: null,
+});
+
 export const Layout = () => {
+  const supashipUserInfo = useSession();
+
   return (
-    <>
+    <UserContext.Provider value={supashipUserInfo}>
       <NavBar />
       <Outlet />
-    </>
+    </UserContext.Provider>
   );
 };
